31a7f0c3cd6e0f54582ac3e4458725c8e153f9fd
HalcyonJX <502229027@qq.com>
2026-01-06 01:25:12 +0800

    用户模块

--- src/main/java/com/halcyon/ainocodebackend/service/UserService.java
+++ src/main/java/com/halcyon/ainocodebackend/service/UserService.java
@@ -1,7 +1,15 @@
 package com.halcyon.ainocodebackend.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.halcyon.ainocodebackend.model.dto.user.UserQueryRequest;
 import com.halcyon.ainocodebackend.model.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.halcyon.ainocodebackend.model.vo.LoginUserVO;
+import com.halcyon.ainocodebackend.model.vo.UserVO;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
 
 /**
 * @author 张嘉鑫
@@ -28,4 +36,60 @@ public interface UserService extends IService<User> {
      * @return
      */
     String getEncryptPassword(String userPassword);
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 获取当前登录用户
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUser(HttpServletRequest request);
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
+     * 用户注销
+     *
+     * @param request
+     * @return
+     */
+    boolean userLogout(HttpServletRequest request);
+
+    /**
+     * 获得脱敏后的用户信息
+     *
+     * @param user
+     * @return
+     */
+    UserVO getUserVO(User user);
+
+    /**
+     * 获得脱敏后的用户信息列表
+     *
+     * @param userList
+     * @return 脱敏后的用户列表
+     */
+    List<UserVO> getUserVOList(List<User> userList);
+
+    /**
+     * 获取查询条件
+     * @param userQueryRequest
+     * @return
+     */
+    QueryWrapper<User> getQueryWrapper(UserQueryRequest userQueryRequest);
 }
